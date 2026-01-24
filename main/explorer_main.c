956dd061aa7ac0671e004e24cd99f87ba147012a
LAB <lazarevski.lazaro@gmail.com>
2026-01-24 22:31:35 +0100

    Introduction code, buildable, with aliveness LED blinking

--- main/explorer_main.c
+++ main/explorer_main.c
@@ -1,7 +1,6 @@
 /*
 
     This is starting point for this event.
-    It's 
 
 */
 
@@ -13,14 +12,18 @@
 #include "driver/gpio.h"
 #include <max7219.h>
 
-#define TAG "APP"
+#define TAG "EXPLORER"
+
+#define ONBOARD_LED GPIO_NUM_2
 
 /* Task configuration*/
 #define TASK_STACK_SIZE 2048
 #define TASK_PRIO_HIGH (configMAX_PRIORITIES - 1)
-#define TASK_PRIO_LOW (configMAX_PRIORITIES - 3)
-#define TASK_PERIOD_10MS 10
-#define TASK_PERIOD_20MS 20
+#define TASK_PRIO_LOW (configMAX_PRIORITIES - 5)
+#define TASK_PRIO_SYS (configMAX_PRIORITIES - 10)
+#define TASK_PERIOD_10MS    10
+#define TASK_PERIOD_20MS    20
+#define TASK_PERIOD_ALIVE   500
 
 /*
     Function declarations
@@ -29,18 +32,48 @@ static void system_init(void);
 static void task_10ms_high_prio(void *pvParameters);
 static void task_20ms_low_prio(void *pvParameters);
 
+static void aliveness_task(void *pvParameters);
 
-/*
-    Initialization, called at system start
 
-    Place here all system initialisation
-*/
-static void system_init(void)
+void led_init(void)
 {
+    ESP_LOGI(TAG, "LED initialization");
 
+    /* Configuration of the system aliveness LED*/
+    gpio_reset_pin(ONBOARD_LED);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(ONBOARD_LED, GPIO_MODE_OUTPUT);
 }
 
 
+/*
+*   Runnable for system allivenes blinking
+*/
+void aliveness_led()
+{
+    static uint8_t sys_led_state = 0x00;
+
+    gpio_set_level(ONBOARD_LED, sys_led_state);
+    /* Toggle LED state for the next call*/
+    sys_led_state = !sys_led_state;
+
+}
+
+/*
+*    Initialization, called at system start
+*    Place here all system initialisation
+*/
+static void system_init(void)
+{
+    
+    /* Initialize all LED */
+    led_init();
+
+}
+
+/*
+* High priority task called every 10ms
+*/
 static void task_10ms_high_prio(void *pvParameters)
 {
     TickType_t task_called_time = xTaskGetTickCount();
@@ -55,6 +88,9 @@ static void task_10ms_high_prio(void *pvParameters)
 
 }
 
+/*
+* Low priority task called every 10ms
+*/
 static void task_20ms_low_prio(void *pvParameters)
 {
     TickType_t task_called_time = xTaskGetTickCount();
@@ -63,26 +99,47 @@ static void task_20ms_low_prio(void *pvParameters)
     {
         /* Call your runnables here*/
 
-        /* Set next call in 10ms */
+        /* Set next call in 20ms */
         vTaskDelayUntil(&task_called_time, pdMS_TO_TICKS(TASK_PERIOD_20MS));
     }
 
 }
 
+/*
+* Task used for showing alivenes of the system
+*/
+static void aliveness_task(void *pvParameters)
+{
+    TickType_t task_called_time = xTaskGetTickCount();
 
+    while(1)
+    {
+        /* Call your runnables here*/
+        aliveness_led();
+
+        /* Set next call in 500ms */
+        vTaskDelayUntil(&task_called_time, pdMS_TO_TICKS(TASK_PERIOD_ALIVE));
+    }
+    
+}
+
+
+/*
+* Main function used for initialization of the system and task creation
+*/
 void app_main(void)
 {
     /* Start with initialization */
 
-    printf("System is booting up \n");
+    ESP_LOGI(TAG,"System is booting up \n");
     system_init();
-    printf("System should be initialized \n");
+    ESP_LOGI(TAG,"System should be initialized \n");
 
-    /* Create high prio 10ms task*/
+    /* Create all tasks*/
     xTaskCreate(task_10ms_high_prio,"high_prio_10ms",TASK_STACK_SIZE,NULL,TASK_PRIO_HIGH,NULL);
-
     xTaskCreate(task_20ms_low_prio,"low_prio_20ms",TASK_STACK_SIZE,NULL,TASK_PRIO_LOW,NULL);
+    xTaskCreate(aliveness_task,"aliveness",TASK_STACK_SIZE,NULL,TASK_PRIO_SYS,NULL);
 
-    printf("Tasks are created, system started \n");
+    ESP_LOGI(TAG,"Tasks are created, system started \n");
 
 }
